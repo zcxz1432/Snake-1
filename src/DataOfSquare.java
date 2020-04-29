b5092284fe5585b9b239ccdfda2c253d6fa2e90c
mijeong <zcxz1432@naver.com>
2020-04-29 23:27:47 +0900

    create new color energy, rock

--- src/DataOfSquare.java
+++ src/DataOfSquare.java
@@ -6,18 +6,21 @@ public class DataOfSquare {
 	
 	//ArrayList that'll contain the colors
 	ArrayList<Color> C =new ArrayList<Color>();
-	int color; //2: snake , 1: food, 0:empty 
+	int color; //4 energy, //3 rock, //2: snake , 1: food, 0:empty 
 	SquarePanel square;
 	public DataOfSquare(int col){
 		
 		//Lets add the color to the arrayList
-		C.add(Color.darkGray);//0
-		C.add(Color.BLUE);    //1
-		C.add(Color.white);   //2
+		C.add(Color.lightGray);//0
+		C.add(Color.YELLOW);    //1
+		C.add(Color.BLACK); //2
+		C.add(Color.RED); //3
+		C.add(Color.BLUE); //4
 		color=col;
 		square = new SquarePanel(C.get(color));
 	}
 	public void lightMeUp(int c){
 		square.ChangeColor(C.get(c));
+		
 	}
 }
